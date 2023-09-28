2ae6b8154cc546079cefdc2041fe63c2f48ef562
ryo-ito1993 <ryo.ito1993@gmail.com>
2023-09-28 13:02:01 +0900

    seeder,header,TOP作成

---
+++ resources/views/rooms.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.header')
+@section('title', 'TOP')
+@section('content')
+
+<div>
+    <h2>客室一覧</h2>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('top');
+})->name('top');
+
+Route::get('/access', function () {
+    return view('access');
+})->name('access');
+
+Route::get('/rooms', function () {
+    return view('rooms');
+})->name('rooms');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +36,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
