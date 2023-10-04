8d5efc78fe25543af5ff3d770909d27a9606dc70
ryo-ito1993 <ryo.ito1993@gmail.com>
2023-10-04 10:48:23 +0900

    前日リマインドメール

---
+++ resources/views/reservation/remind_mail.blade.php
@@ -0,0 +1,7 @@
+{{ $last_name}} {{ $first_name }}様<br>
+ご予約日が明日となりましたので、お知らせいたします。<br>
+<br>
+予約日：{{ $reservation_date }}<br>
+プラン：{{ $plan_title }}<br>
+部屋タイプ：{{ $room_type }}<br>
+金額：￥{{ $price }}
