6f277a4a7d0a3504fa86c2484082665796bdca88
ryo-ito1993 <ryo.ito1993@gmail.com>
2023-10-04 23:53:58 +0900

    予約管理画面検索機能

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -5,17 +5,59 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Reservation;
+use App\Models\Plan;
+use App\Models\Room;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ReservationCancelmail;
 use Illuminate\Support\Facades\DB;
 
 class ReservationController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $reservations = Reservation::with('plan', 'reservation_slot.room')->get();
-        return view('admin.reservations.index', compact('reservations'));
+        $name = $request->input('name');
+        $date = $request->input('date');
+        $email = $request->input('email');
+        $plan = $request->input('plan');
+        $room = $request->input('room');
+        $status = $request->input('status');
+
+        $plans = Plan::all();
+        $rooms = Room::all();
+
+        $reservations = Reservation::with('plan', 'reservation_slot.room')
+            ->when($name, function ($query) use ($name) {
+                $query->where(function ($query) use ($name) {
+                    $query->where('last_name', 'like', "%{$name}%")
+                            ->orWhere('first_name', 'like', "%{$name}%");
+                });
+            })
+            ->when($email, function ($query) use ($email) {
+                $query->where('email', 'like', "%{$email}%");
+            })
+            ->when($plan, function ($query) use ($plan) {
+                $query->whereHas('plan', function ($query) use ($plan) {
+                    $query->where('id', $plan);
+                });
+            })
+            ->when($room, function ($query) use ($room) {
+                $query->whereHas('reservation_slot.room', function ($query) use ($room) {
+                    $query->where('id', $room);
+                });
+            })
+            ->when($date, function ($query) use ($date) {
+                $query->whereHas('reservation_slot', function ($query) use ($date) {
+                    $query->where('date', $date);
+                });
+            })
+            ->when($status !== null, function ($query) use ($status) {
+                $query->where('status', $status);
+            })
+            ->get();
+        return view('admin.reservations.index', compact('reservations', 'plans', 'rooms'));
     }
+
+
 
     public function show(Reservation $reservation)
     {
--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -12,7 +12,52 @@
             {{ session('success') }}
         </div>
     @endif
-    <table class="table">
+    <button class="btn btn-info" type="button" data-toggle="collapse" data-target="#searchForm" aria-expanded="false" aria-controls="searchForm">
+        検索フォームを開く/閉じる
+    </button>
+    <div class="collapse" id="searchForm">
+        <div class="card card-body">
+            <form action="{{ route('admin.reservations.index') }}" method="GET">
+                <div class="mb-3">
+                    <input type="text" name="name" id="name" class="form-control" placeholder="名前で検索" value="{{ request('name') }}">
+                </div>
+                <div class="mb-3">
+                    <input type="text" name="email" id="email" class="form-control" placeholder="メールアドレスで検索" value="{{ request('email') }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="date">予約日</label>
+                    <input type="date" name="date" id="date" class="form-control" value="{{  request('date') }}">
+                </div>
+                <div class="form-group">
+                    <select name="plan" id="plan" class="form-control">
+                        <option disabled selected value>プランを選択してください</option>
+                        @foreach($plans as $plan)
+                            <option value="{{ $plan->id }}" {{  request('plan') == $plan->id ? 'selected' : '' }}>{{ $plan->title }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select name="room" id="room" class="form-control">
+                        <option disabled selected value>部屋タイプを選択してください</option>
+                        @foreach($rooms as $room)
+                            <option value="{{ $room->id }}" {{  request('room') == $room->id ? 'selected' : '' }}>{{ $room->type }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select name="status" id="status" class="form-control">
+                        <option disabled selected value>予約状況を選択してください</option>
+                        <option value="0" {{ request('status') === "0" ? 'selected' : '' }}>予約済</option>
+                        <option value="1" {{ request('status') === "1" ? 'selected' : '' }}>キャンセル</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-secondary">検索</button>
+            </form>
+        </div>
+    </div>
+
+    <table class="table mt-3">
         <thead>
             <tr>
                 <th>ID</th>
